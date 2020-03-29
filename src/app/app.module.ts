dd247bd3c41a812ff2e1b71526bc10edd37bc70b
MustafoVohidi <48031070+MustafoVohidi@users.noreply.github.com>
2020-03-29 11:46:43 +0300

    commit only changed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestsComponent } from './tests/tests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestsComponent
   ],
   imports: [
     BrowserModule
